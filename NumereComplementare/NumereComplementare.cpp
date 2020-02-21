974953ef0b0544658a277b1d38f769b55acd0f00
Sorin Carcalicea <Sorin.Carcalicea@endava.com>
2020-02-21 17:25:30 +0200

    Read files into vectors...

--- NumereComplementare/NumereComplementare.cpp
+++ NumereComplementare/NumereComplementare.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <fstream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+ifstream inputFile("numere.in");
+ofstream outfile("numere.out");
+
+int main() {
+
+    if (inputFile) {
+        int nr = 0;
+        int numbers[6];
+        int cifre[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        inputFile >> nr;
+
+        for (int i = 0; i < 6; i++) {
+            inputFile >> numbers[i];
+        }
+
+    } else {
+        cout << "Error opening file!!!" << endl;
+        return -1;
+    }
+    
+    return 0;
 }
