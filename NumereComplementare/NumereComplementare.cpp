a2c12684e4de512d0e35d90155c9270b3262d3e3
Sorin Carcalicea <Sorin.Carcalicea@endava.com>
2020-02-25 09:09:11 +0200

    Implemented all requirements Odd and even digit issue remains.

--- NumereComplementare/NumereComplementare.cpp
+++ NumereComplementare/NumereComplementare.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include "NumereComplementare.h"
 
 using namespace std;
 
@@ -10,14 +12,78 @@ int main() {
 
     if (inputFile) {
         int nr = 0;
-        int numbers[6];
-        int cifre[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        vector<int> numbers;
+        vector<int> cifre {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        vector<int> complementNrs;
 
+        // Read numbers into vectors
         inputFile >> nr;
-
-        for (int i = 0; i < 6; i++) {
-            inputFile >> numbers[i];
+        int temp;
+        while (inputFile >> temp) {
+            numbers.push_back(temp);
         }
+        
+        // Find complementary numbers: 3 steps
+        // 1. remove elements from cifre that are contained in our number
+        int origNr = nr;
+        int cifra = origNr % 10;
+        while (cifra != 0) {
+            for (auto j = cifre.begin(); j != cifre.end(); ++j) {
+                if (cifra == *j) {
+                    cifre.erase(j);
+                    break;
+                }
+            }
+            origNr = origNr / 10;
+            cifra = origNr % 10;
+        }
+
+        // 2. find out complementary numbers
+        for (auto i = numbers.begin(); i != numbers.end(); ++i) {
+            int complement = *i;
+            int digit = complement % 10;
+
+            if (complement > nr) {
+                do {
+
+                    // Find out if digit from complement is an allowed digit
+                    bool digitOk = false;
+                    for (auto j = cifre.begin(); j != cifre.end(); ++j) {
+                        if (digit == *j) { // Digit not allowed
+                            digitOk = true;
+                            break;
+                        }
+                    }
+
+                    // Do not evaluate further
+                    if (!digitOk) { 
+                        digit = -1;
+                        break;
+                    }
+
+                    complement = complement / 10;
+                    digit = complement % 10;
+                } while (complement > 0);
+
+                // Complement found, save it into vector
+                if (digit != -1) {
+                    complementNrs.push_back(*i);
+                }
+            }
+        }
+
+        // 3. find out if the number of even digits is equal with the number of odd digits for both number and complementary number (remove complementary number if otherwhise)
+        //calculateOddAndEvenDigits(complementNrs, nr);
+
+        // 4. find the larger number of complement and print it out
+        int max = 0;
+        for (auto it = complementNrs.begin(); it != complementNrs.end(); ++it) {
+            if (max < *it) {
+                max = *it;
+            }
+        }
+
+        cout << "Largest number is: " << max << endl;
 
     } else {
         cout << "Error opening file!!!" << endl;
@@ -25,4 +91,50 @@ int main() {
     }
     
     return 0;
+}
+
+// find out if the number of even digits is equal with the number of odd digits for both numberand complementary number
+// (remove complementary number if otherwhise)
+void calculateOddAndEvenDigits(std::vector<int>& complementNrs, int nr)
+{
+    int odd = 0;
+    int even = 0;
+    for (auto it = complementNrs.begin(); it != complementNrs.end(); ++it) {
+
+        // Evaluate complement
+        int complement = *it;
+        int digit = complement % 10;
+        do {
+            if (digit % 2 == 0) {
+                even++;
+            }
+            else {
+                odd++;
+            }
+            complement = complement / 10;
+            digit = complement % 10;
+        } while (complement > 0);
+
+
+        // Evaluate number
+        int number = nr;
+        digit = number % 10;
+        do {
+            if (digit % 2 == 0) {
+                even--;
+            }
+            else {
+                odd--;
+            }
+            number = number / 10;
+            digit = number % 10;
+        } while (number > 0);
+
+        // The number of even or odd digits is not the same in both numbers
+        if (odd != 0 || even != 0) {
+            complementNrs.erase(it);
+            it--;
+        }
+
+    }
 }
---
+++ NumereComplementare/NumereComplementare.h
@@ -0,0 +1,3 @@
+#pragma once
+
+void calculateOddAndEvenDigits(std::vector<int>& complementNrs, int nr);
